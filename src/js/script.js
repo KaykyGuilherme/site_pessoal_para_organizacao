eaa0c6d724a9679581ed88a0075ddb2792c66e8e
Kayky Guilherme <130869882+KaykyGuilherme@users.noreply.github.com>
2024-02-09 17:20:55 -0300

    2

--- src/js/script.js
+++ src/js/script.js
@@ -84,3 +84,72 @@ const calendarioTempoReal = setInterval(() =>{
     calendarioALL.mesNumber.textContent = mesNumero
     calendarioALL.diaNumber.textContent = diaMes
 }, 86.400000)
+
+//=================================================
+//tempo
+
+const keyWeather = '46108e434e2e9a283a3c0414bfdf088b'
+
+const cardWeather = {
+    cardBottom: document.querySelector('.weather__bottom'),
+    cidyHTML: document.querySelector('#cidade__weatherID'),
+    btn: document.querySelector('.weather__top button'),
+    nameCidyHTML: document.querySelector('.infos__name__city h3'),
+    pandeira: document.querySelector('.infos__name__city img'),
+    graus: document.querySelector('.graus'),
+    estadoEscrito: document.querySelector('.estado__tempo p'),
+    estadoIMG: document.querySelector('.estado__tempo img'),
+    agua: document.querySelector('.agua'),
+    vento: document.querySelector('.vento'),
+
+    erro404: document.querySelector('.weather404')
+}
+
+cardWeather.btn.addEventListener('click', () =>{
+    pegarWeatherAPI(cardWeather.cidyHTML.value)
+})
+
+cardWeather.cidyHTML.addEventListener('keyup', (evento) =>{
+    if(evento.key == 'Enter'){
+        pegarWeatherAPI(cardWeather.cidyHTML.value)
+    }
+})
+
+const pegarWeatherAPI = async (cidy) =>{
+
+    const apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${cidy}&units=metric&&appid=${keyWeather}&lang=pt_br`
+
+    const res = await fetch(apiLink)
+    const data = await res.json()
+    
+    mudarCard(data)
+}
+
+const mudarCard = (data) =>{
+    console.log(data)
+    if(data.cod == '404'){
+        cardWeather.cardBottom.classList.add('weather__hide')
+        cardWeather.erro404.classList.remove('weather__hide')
+    }else{
+        if(cardWeather.cardBottom.classList.contains('weather__hide')){
+            cardWeather.cardBottom.classList.remove('weather__hide')
+            cardWeather.erro404.classList.add('weather__hide')
+        }
+        cardWeather.nameCidyHTML.textContent = data.name
+        cardWeather.pandeira.src = `https://flagsapi.com/${data.sys.country}/shiny/64.png`
+        cardWeather.graus.textContent = `${Math.floor(data.main.temp)}°`
+        cardWeather.estadoEscrito.textContent = data.weather[0].description
+        cardWeather.estadoIMG.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    
+        cardWeather.agua.innerHTML = `
+        <i class="fa-solid fa-droplet"></i>
+        ${data.main.humidity}%
+        `
+    
+        cardWeather.vento.innerHTML = `
+        <i class="fa-solid fa-wind"></i>
+        ${data.wind.speed}km/h`
+    }
+}
+
+pegarWeatherAPI('franca')
